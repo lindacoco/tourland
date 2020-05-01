1f4785d148b4c7f126c1fc99e2d2b9e2d2256e3d
lindacoco <lindacoco0204@gmail.com>
2020-05-01 22:07:38 +0900

    --

--- src/main/java/com/yi/tourland/controller/mng/ManagerController.java
+++ src/main/java/com/yi/tourland/controller/mng/ManagerController.java
@@ -18,7 +18,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
