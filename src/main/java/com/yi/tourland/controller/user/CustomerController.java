09e3f10af8c6a7a5759400a0f5edca2a0ad58203
lindacoco <lindacoco0204@gmail.com>
2020-05-09 21:39:46 +0900

    팝업 구현 완료, 쿠키 저장

--- src/main/java/com/yi/tourland/controller/user/CustomerController.java
+++ src/main/java/com/yi/tourland/controller/user/CustomerController.java
@@ -1,15 +1,84 @@
 package com.yi.tourland.controller.user;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.yi.tourland.domain.mng.PopupVO;
+import com.yi.tourland.service.mng.BannerService;
+import com.yi.tourland.service.mng.CouponService;
+import com.yi.tourland.service.mng.CustBoardService;
+import com.yi.tourland.service.mng.EmployeeService;
+import com.yi.tourland.service.mng.FaqService;
+import com.yi.tourland.service.mng.FlightService;
+import com.yi.tourland.service.mng.HotelService;
+import com.yi.tourland.service.mng.NoticeService;
+import com.yi.tourland.service.mng.PopupService;
+import com.yi.tourland.service.mng.RentcarService;
+import com.yi.tourland.service.mng.TourService;
+import com.yi.tourland.service.mng.UserService;
+
 @Controller
 public class CustomerController {
+	@Resource(name = "uploadPath") // 서블릿컨텍스트의 id값과 일치해야함
+	private String uploadPath; // c:/tourland/upload
+	
+	@Resource(name = "uploadPath2")
+	private String uploadPathPopup; // c:/tourland/upload/popup
+
+	@Autowired
+	private TourService tourService;
+	@Autowired
+	private FaqService faqService;
+
+	@Autowired
+	NoticeService noticeService;
+
+	@Autowired
+	CustBoardService custBoardService;
+
+	@Autowired
+	CouponService couponService;
+
+	@Autowired
+	EmployeeService employeeService;
+
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	HotelService hotelService;
+	
+	@Autowired
+	PopupService popupService;
+
+	@Autowired
+	BannerService bannerService;
+
+	@Autowired
+	FlightService flightService;
+
+	@Autowired
+	RentcarService rentcarService;
+	
 	
 	//메인
 	@RequestMapping(value="tourlandMain", method=RequestMethod.GET)
-	public String tourlandMain() { 
+	public String tourlandMain(Model model) throws Exception {
+		//팝업 불러오기
+		PopupVO popup1 = popupService.setPopup("L");
+		if(popup1 != null) {
+			model.addAttribute("popup1",popup1.getPic());
+		}
+		PopupVO popup2 = popupService.setPopup("R");
+		if(popup2 != null) {
+			model.addAttribute("popup2",popup2.getPic());
+		}
+		
 		return "/user/tourlandMain"; 
 	}
 	//마이 페이지 - 내 정보 수정
